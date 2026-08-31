83f37e15746055ed613133cf5cf0ea18ad207b02
Ananta Rajegore <anantarajegore@gmail.com>
2026-08-31 08:24:02 +0530

    Daily problems

--- MyPractice/java.java
+++ MyPractice/java.java
@@ -1102,28 +1102,41 @@ public class java {
     //     System.out.println(isPalindrome(x));
     // }
 
-    //41)Search insert element-bineary search
-    public static int search(int nums[], int target){
-        int mid ;
-        int left = 0;
-        int right = nums.length - 1;
-        while(left <= right){
-            mid = left + (right - left)/2;
-            if(nums[mid] == target){
-                return mid;
-            }else if(target < nums[mid]){
-                right = mid - 1;
-            }else{
-                left = mid + 1;
-            }
-        }
-        return left;
+    // //41)Search insert element-bineary search
+    // public static int search(int nums[], int target){
+    //     int mid ;
+    //     int left = 0;
+    //     int right = nums.length - 1;
+    //     while(left <= right){
+    //         mid = left + (right - left)/2;
+    //         if(nums[mid] == target){
+    //             return mid;
+    //         }else if(target < nums[mid]){
+    //             right = mid - 1;
+    //         }else{
+    //             left = mid + 1;
+    //         }
+    //     }
+    //     return left;
+    // }
+    //  public static void main(String[] args) {
+    //     int nums[] = {1, 2, 4, 6, 8};
+    //     int target = 3;
+    //     System.out.println(search(nums,target));
+    //  }
+
+    //42)Length of last word
+
+    public static int lengthOfLastWord(String s) {
+        String words[] = s.split(" ");
+        String lastWord = words[words.length - 1];
+        int length = lastWord.length();
+        return length;
     }
-     public static void main(String[] args) {
-        int nums[] = {1, 2, 4, 6, 8};
-        int target = 3;
-        System.out.println(search(nums,target));
-     }
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(lengthOfLastWord(s));
+    }
 
      
 }
