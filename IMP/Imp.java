388f97f8301ee4af2504ffa912df8de6f52d9994
Ananta Rajegore <anantarajegore@gmail.com>
2026-09-16 08:15:17 +0530

    New Imp questions series start

---
+++ IMP/Imp.java
@@ -0,0 +1,45 @@
+package IMP;
+import java.util.*;
+
+public class Imp {
+
+    public static List<List<String>> groupAnagrams(String strs[]){
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String s:strs){
+            int [] count = new int[26];
+
+            for(char c : s.toCharArray()){
+                count[c - 'a']++;
+            }
+             StringBuilder sb = new StringBuilder();
+             for(int i:count){
+                sb.append("#");
+                sb.append(i);
+             }
+
+             String key = sb.toString();
+
+             if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<String>());
+             }
+
+             map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    } 
+    public static void main(String[] args) {
+         String[] strs = {
+            "eat",
+            "tea",
+            "tan",
+            "ate",
+            "nat",
+            "bat"
+        };
+
+        System.out.println(groupAnagrams(strs));
+
+    }
+}
